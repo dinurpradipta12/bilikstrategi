f153a9c4389c331c0a8b4083c98e2a03ec9a5f2e
Dinur Pradipta <dinurm.pradipta@gmail.com>
2026-08-03 16:17:24 +0800

    Configure project detail for edge runtime

--- app/(dashboard)/projects/[id]/page.tsx
+++ app/(dashboard)/projects/[id]/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const runtime = 'edge';
+
 import React, { useState, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import { useParams } from 'next/navigation';
